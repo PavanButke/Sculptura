78e63af3ff27ff5f5500c8a1b9e37995300119af
PavanButke <butkepavan55@gmail.com>
2022-08-05 13:02:00 +0530

    update profile

--- Server/Routes/UserRouters.js
+++ Server/Routes/UserRouters.js
@@ -88,21 +88,21 @@ userRouter.get(
     })
 );
 
-// LOGIN
-export const userLoginReducer = (state = {}, action) => {
-    switch (action.type) {
-        case USER_LOGIN_REQUEST:
-            return { loading: true };
-        case USER_LOGIN_SUCCESS:
-            return { loading: false, userInfo: action.payload };
-        case USER_LOGIN_FAIL:
-            return { loading: false, error: action.payload };
-        case USER_LOGOUT:
-            return {};
-        default:
-            return state;
-    }
-};
+// // LOGIN
+// export const userLoginReducer = (state = {}, action) => {
+//     switch (action.type) {
+//         case USER_LOGIN_REQUEST:
+//             return { loading: true };
+//         case USER_LOGIN_SUCCESS:
+//             return { loading: false, userInfo: action.payload };
+//         case USER_LOGIN_FAIL:
+//             return { loading: false, error: action.payload };
+//         case USER_LOGOUT:
+//             return {};
+//         default:
+//             return state;
+//     }
+// };
 
 // UPDATE PROFILE
 userRouter.put(
