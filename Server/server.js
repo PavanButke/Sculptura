2a16ef93fd6ffef9f663a4d6098164ff24d5b892
PavanButke <butkepavan55@gmail.com>
2022-08-17 17:51:53 +0530

    trying razorpay integration

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import dotenv from "dotenv";
 import connectDatabase from "./config/Mongodb.js";
 import ImportData from "./DataImport.js"
@@ -7,12 +6,19 @@ import productRoute from './Routes/ProductRoute.js';
 import { errorHandler, notFound } from "./Middleware/Errors.js";
 import userRouter from './Routes/UserRoutes.js';
 import orderRouter from "./Routes/OrderRoutes.js";
-
+import Razorpay from 'razorpay';
+import paymentRoutes from './Routes/paymentRoutes.js';
+import cors from "cors";
 
 dotenv.config();
 connectDatabase();
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 
 //API Call
 app.use("/api/import", ImportData);
@@ -24,6 +30,7 @@ app.use("/api/config/paypal", (req,res)=>{
 })
 
 
+
 app.use(notFound)
 app.use(errorHandler)
 
@@ -32,6 +39,7 @@ app.get("/", (req, res) => {
 res.send("API is Running ...");
 });
 
-const PORT = process.env.PORT|| 1000;
 
+
+  const PORT = process.env.PORT|| 1000;
 app.listen(PORT, console.log(`server run in port ${PORT}`));
